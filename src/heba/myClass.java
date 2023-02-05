626565b0f6bfa5b439003ccb8e256540852b3e92
asaqa988 <asaqa001@gmail.com>
2023-02-05 18:30:13 +0300

    updarteteljhdslfhsjfkhsdkfjshfhdskfhs

---
+++ src/heba/myClass.java
@@ -0,0 +1,95 @@
+package heba;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class myClass {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+
+		/*
+		 * String , double , int , boolean , date , a , main programming - (for , if ,
+		 * while , break , countinue ) methods ( string methods , Math) 
+		 * oop ,
+		 * inheretance , object
+		 */
+
+//		String myName = "abedalraheem";
+//
+//		String myStudent;
+//
+//		myStudent = "heba";
+
+//		System.out.println(myName.contains("z"));
+//		System.out.println(myName.contains("a"));
+
+		;
+
+//		System.out.println(myName.toUpperCase().charAt(1));
+
+//		System.out.println(myName.concat("faisal"));
+
+//		String zzz = myName+" "+"faisal"; 
+//		
+//		System.out.println(zzz);
+
+//		double myDouble = -88.9;
+//	System.out.println(Math.round(myDouble));	
+//	System.out.println(Math.ceil(myDouble));
+//	System.out.println(Math.floor(myDouble));
+
+//System.out.println(Math.absExact((int) myDouble));
+//; 
+//System.out.println(Math.abs(myDouble));
+//
+//		boolean iLoveMySelf = false;
+//
+//		Date myTime = new Date();
+
+//		System.out.println(myTime);
+
+//		myTime.getHours();
+		
+		// ----------------Part 2 ----------------------
+//		for () {}	
+//		while () {}
+//		if() {}
+		
+		
+//		for(int i = 0 ; i < 100 ; i ++) {
+//			if (i%2==1) {
+//				if(i ==9 ||i==19||i==29||i==39) {
+//					continue; 
+//					break;
+//				}
+//			
+//
+//				}
+//				System.out.println(i);
+//
+//			}
+		int i = 3 ; 
+	
+		while(i == 3) {
+			
+			System.out.println(i);
+			i++ ; 
+		}
+		
+		System.out.println(i);
+	
+
+	}
+
+
+
+}
